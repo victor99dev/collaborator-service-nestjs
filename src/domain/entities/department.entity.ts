cf9406d23c457854e224f2f0c46c24db9f8a55ec
Victor Hugo <victormotabatista@gmail.com>
2023-02-05 01:01:17 -0300

    fix(domain): entity-had-no-name-field

--- src/domain/entities/department.entity.ts
+++ src/domain/entities/department.entity.ts
@@ -15,8 +15,8 @@ export class Departments {
   private props: DepartmentsProps;
 
   constructor(
-    id: string,
     props: Replace<DepartmentsProps, { createdAt?: Date }>,
+    id?: string,
   ) {
     this._id = id ?? randomUUID();
     this.props = {
@@ -27,6 +27,13 @@ export class Departments {
 
   public get id() {
     return this._id;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+  public set name(name: string) {
+    this.props.name = name;
   }
 
   public get description(): string {
