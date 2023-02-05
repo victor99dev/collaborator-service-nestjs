d38e30850bb8a80dcb5f422bdf332eba9d618659
Victor Hugo <victormotabatista@gmail.com>
2023-02-04 21:17:28 -0300

    feat(domain): creating-collaborators-entity

---
+++ src/domain/entities/collaborators.entity.ts
@@ -0,0 +1,159 @@
+import { randomUUID } from 'crypto';
+import { Address, Documents, SocialMedia } from '../value-object';
+import { Departments } from './department.entity';
+import { Replace } from 'src/helpers';
+
+export enum GroupsType {
+  CLT = 'clt',
+  PJ = 'PJ',
+  FREELANCER = 'freelancer',
+  PARTNER = 'partner',
+}
+
+export interface CollaboratorsProps {
+  name: string;
+  email: string;
+  age: number;
+  documents: Documents[];
+  department: Departments[];
+  group: GroupsType[];
+  address?: Address | null;
+  socialMedia?: SocialMedia[] | null;
+  login: string;
+  password: string;
+  status: boolean;
+  description?: string | null;
+  active: boolean;
+  isDeleted: boolean;
+  createdAt: Date;
+  updatedAt?: Date | null;
+}
+
+export class Collaborators {
+  private _id: string;
+  private props: CollaboratorsProps;
+
+  constructor(
+    props: Replace<CollaboratorsProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
+  public get email(): string {
+    return this.props.email;
+  }
+  public set email(email: string) {
+    this.props.email = email;
+  }
+
+  public get age(): number {
+    return this.props.age;
+  }
+  public set age(age: number) {
+    this.props.age = age;
+  }
+
+  public get documents(): Documents[] {
+    return this.props.documents;
+  }
+  public set documents(documents: Documents[]) {
+    this.props.documents = documents;
+  }
+
+  public get department(): Departments[] {
+    return this.props.department;
+  }
+  public set department(department: Departments[]) {
+    this.props.department = department;
+  }
+
+  public get group(): GroupsType[] {
+    return this.props.group;
+  }
+  public set group(group: GroupsType[]) {
+    this.props.group = group;
+  }
+
+  public get address(): Address {
+    return this.props.address;
+  }
+  public set address(address: Address) {
+    this.props.address = address || null;
+  }
+
+  public get socialMedia(): SocialMedia[] {
+    return this.props.socialMedia;
+  }
+  public set socialMedia(socialMedia: SocialMedia[]) {
+    this.props.socialMedia = socialMedia || null;
+  }
+
+  public get login(): string {
+    return this.props.login;
+  }
+  public set login(login: string) {
+    this.props.login = login;
+  }
+
+  public get password(): string {
+    return this.props.password;
+  }
+  public set password(password: string) {
+    this.props.password = password;
+  }
+
+  public get status(): boolean {
+    return this.props.status;
+  }
+  public set status(status: boolean) {
+    this.props.status = status;
+  }
+
+  public get description(): string {
+    return this.props.description;
+  }
+  public set description(description: string) {
+    this.props.description = description || null;
+  }
+
+  public get active(): boolean {
+    return this.props.active;
+  }
+  public set active(active: boolean) {
+    this.props.active = active;
+  }
+
+  public get isDeleted(): boolean {
+    return this.props.isDeleted;
+  }
+  public set isDeleted(isDeleted: boolean) {
+    this.props.isDeleted = isDeleted;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+  public update() {
+    this.props.updatedAt = new Date() || null;
+  }
+}
