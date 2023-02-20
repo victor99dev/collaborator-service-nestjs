2ccceb529045410484588a774dcd1f67825d7f9b
Victor Hugo <victormotabatista@gmail.com>
2023-02-20 00:28:47 -0300

    docs(swagger): configuration swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('collaborator-service')
+    .setDescription('employee creation service')
+    .setVersion('1.0.0')
+    .setContact('Victor99dev', 'https://victor99dev.site/', '')
+    .setLicense(
+      'Copyright (c) 2023, Victor99dev, Todos os direitos reservados..',
+      'https://github.com/torugo99',
+    )
+    .setExternalDoc('Open Api: swagger-ui-json', '/swagger-ui-json')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger-ui', app, document);
+
+  app.enableCors();
   await app.listen(3000);
 }
+
 bootstrap();
