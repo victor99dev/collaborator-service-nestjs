1a140c06c0db25f661790e8934d4d362cddf75cc
Victor Hugo <victormotabatista@gmail.com>
2023-03-17 22:38:54 -0300

    feat(dto): ajusting dto

--- src/infra/http/modules/departments/dtos/update-department.dto.ts
+++ src/infra/http/modules/departments/dtos/update-department.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class RegisterDepartmentDto {
+export class UpdateDepartmentDto {
   @ApiProperty()
   name: string;
 
@@ -9,9 +9,6 @@ export class RegisterDepartmentDto {
 
   @ApiProperty()
   active: boolean;
-
-  @ApiProperty()
-  created_at: Date;
 
   @ApiProperty()
   updated_at: Date;
