2f0b84007cde3aa8af6cccf5b77b6e833825f950
Victor Hugo <victormotabatista@gmail.com>
2023-03-19 06:59:01 -0300

    feat(Delete): creating a new context for Department and inserting its dependencies

--- src/application/usecases/department/delete-department.usecase.ts
+++ src/application/usecases/department/delete-department.usecase.ts
@@ -1,1 +1,18 @@
-export class DeleteDepartmentUsecase {}
+import { Inject, Injectable } from '@nestjs/common';
+import { IDepartmentRepository } from 'src/application/contracts/repository';
+import { TOKENS } from 'src/infra/container';
+@Injectable()
+export class DeleteDepartmentUsecase {
+  constructor(
+    @Inject(TOKENS.repositories.DEPARTMENTS)
+    private readonly _departmentRepository: IDepartmentRepository,
+  ) {}
+
+  async execute(code: string): Promise<void> {
+    const codeReturn = await this._departmentRepository.findByCode(code);
+
+    if (!codeReturn) throw new Error(`Not Found Department with code ${code}`);
+
+    return await this._departmentRepository.delete(code);
+  }
+}
