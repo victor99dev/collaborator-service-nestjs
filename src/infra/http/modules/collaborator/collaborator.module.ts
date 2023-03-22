19d1a69f9a4ad4ab35c4a2b2e7ef8e9c39e950de
Victor Hugo <victormotabatista@gmail.com>
2023-03-22 17:07:19 -0300

    feat(infra): inserting dependencies for collaborator

--- src/infra/http/modules/collaborator/collaborator.module.ts
+++ src/infra/http/modules/collaborator/collaborator.module.ts
@@ -3,7 +3,10 @@ import { CollaboratorService } from './collaborator.service';
 import { CollaboratorController } from './collaborator.controller';
 import { TOKENS } from 'src/infra/container';
 import { IMemoryCollaboratorRepository } from 'src/infra/repositories/in-memory';
-import { CreateCollaboratorUseCase } from 'src/application/usecases/collaborator';
+import {
+  CreateCollaboratorUseCase,
+  ListCollaboratorUseCase,
+} from 'src/application/usecases/collaborator';
 
 @Module({
   controllers: [CollaboratorController],
@@ -12,8 +15,9 @@ import { CreateCollaboratorUseCase } from 'src/application/usecases/collaborator
       provide: TOKENS.repositories.COLLABORATORS,
       useFactory: () => new IMemoryCollaboratorRepository(),
     },
-    CreateCollaboratorUseCase,
     CollaboratorService,
+    CreateCollaboratorUseCase,
+    ListCollaboratorUseCase,
   ],
 })
 export class CollaboratorModule {}
--- src/infra/http/modules/collaborator/collaborator.service.ts
+++ src/infra/http/modules/collaborator/collaborator.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCollaboratorUseCase } from 'src/application/usecases/collaborator';
+import {
+  CreateCollaboratorUseCase,
+  ListCollaboratorOutput,
+  ListCollaboratorUseCase,
+} from 'src/application/usecases/collaborator';
 
 @Injectable()
 export class CollaboratorService {
-  constructor(private _createCollaboratorUseCase: CreateCollaboratorUseCase) {}
+  constructor(
+    private _createCollaboratorUseCase: CreateCollaboratorUseCase,
+    private _listCollaboratorUseCase: ListCollaboratorUseCase,
+  ) {}
 
   async createCollaborator(input: any): Promise<void> {
     return this._createCollaboratorUseCase.execute(input);
+  }
+
+  async getAll(): Promise<ListCollaboratorOutput> {
+    return await this._listCollaboratorUseCase.execute();
   }
 }
