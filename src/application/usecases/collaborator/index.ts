37916756a7d88a86b0e3c6a7f4bf44f776ff5ae0
Victor Hugo <victormotabatista@gmail.com>
2023-03-22 16:54:03 -0300

    feat(usecase): creating the collaborator usecase to show a listing

--- src/application/usecases/collaborator/index.ts
+++ src/application/usecases/collaborator/index.ts
@@ -1,1 +1,2 @@
 export * from './create-collaborator.usecase';
+export * from './list-collaborator.usecase';
---
+++ src/application/usecases/collaborator/list-collaborator.usecase.ts
@@ -0,0 +1,32 @@
+import { Inject } from '@nestjs/common';
+import { ICollaboratorsRepository } from 'src/application/contracts';
+import { TOKENS } from 'src/infra/container';
+import { ListCollaboratorViewModel } from 'src/infra/http/view-models/collaborator';
+
+export class ListCollaboratorUseCase {
+  constructor(
+    @Inject(TOKENS.repositories.COLLABORATORS)
+    private readonly _collaboratorRepository: ICollaboratorsRepository,
+  ) {}
+
+  async execute() {
+    const collaborator = await this._collaboratorRepository.getAll();
+
+    const total = await this._collaboratorRepository.count();
+
+    return {
+      data: ListCollaboratorViewModel.toHttpList(collaborator),
+      metadata: {
+        total,
+        length: collaborator.length,
+      },
+    };
+  }
+}
+
+export type ListCollaboratorOutput = {
+  metadata: {
+    [key: string]: unknown;
+  };
+  data: ListCollaboratorViewModel[];
+};
