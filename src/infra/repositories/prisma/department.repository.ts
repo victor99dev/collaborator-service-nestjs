17d83444375c4e1947a91c8614a596a91e6aeaa1
Victor Hugo <victormotabatista@gmail.com>
2023-03-31 23:02:20 -0300

    refactor(infra): adjusting departments

--- src/infra/repositories/prisma/department.repository.ts
+++ src/infra/repositories/prisma/department.repository.ts
@@ -2,32 +2,39 @@ import { PrismaClient } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
 import { IDepartmentRepository } from 'src/application/contracts';
 import { Departments } from 'src/domain/entities';
-import { PrismaDepartmentMapper } from 'src/infra/mappers/department-mapper';
+import { PrismaDepartmentMapper } from 'src/infra/database/mappers';
 
 @Injectable()
 export class IPrismaDepartmentRepository implements IDepartmentRepository {
   constructor(private readonly _prismaClient: PrismaClient) {}
+
   async save(data: Departments): Promise<void> {
     const departmentMapper = PrismaDepartmentMapper.toPrisma(data);
     await this._prismaClient.departments.create({
       data: { ...departmentMapper },
     });
   }
+
   update(code: string, data: any): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
   findByCode(code: string): Promise<Departments> {
     throw new Error('Method not implemented.');
   }
+
   findByLogin(login: string): Promise<Departments> {
     throw new Error('Method not implemented.');
   }
+
   count(): Promise<number> {
     throw new Error('Method not implemented.');
   }
+
   getAll(): Promise<Departments[]> {
     throw new Error('Method not implemented.');
   }
+
   delete(code: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
