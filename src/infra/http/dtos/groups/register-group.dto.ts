185bbd70f4dd7b2390f13b93d37227472521857f
Victor Hugo <victormotabatista@gmail.com>
2023-05-21 21:34:33 -0300

    feat(validation): adding validations to group fields

--- src/infra/http/dtos/groups/register-group.dto.ts
+++ src/infra/http/dtos/groups/register-group.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, Length } from 'class-validator';
 
 export class RegisterGroupDto {
+  @IsNotEmpty({ message: 'Name is required' })
   @ApiProperty({ type: String })
   name: string;
 
+  @Length(5, 100, {
+    message:
+      'The maximum number of characters in the description is from 5 to 100',
+  })
   @ApiProperty({ type: String })
   description: string;
 
+  @IsBoolean({ message: 'Active is a boolean, use true or false' })
   @ApiProperty({ type: Boolean })
   active: boolean;
 
