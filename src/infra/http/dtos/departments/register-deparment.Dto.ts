766028aad2264a14d93bc024101c7898baa49af3
Victor Hugo <victormotabatista@gmail.com>
2023-05-21 22:19:23 -0300

    feat(validation): adding validations to department fields

--- src/infra/http/dtos/departments/register-deparment.Dto.ts
+++ src/infra/http/dtos/departments/register-deparment.Dto.ts
@@ -1,15 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, Length } from 'class-validator';
 
 export class RegisterDepartmentDto {
+  @IsNotEmpty({ message: 'Department name is required' })
   @ApiProperty({ type: String })
   name: string;
 
+  @Length(5, 100, {
+    message:
+      'The maximum number of characters in the description is from 5 to 100',
+  })
   @ApiProperty({ type: String })
   description: string;
 
+  @IsBoolean({ message: 'Active is a boolean, use true or false' })
   @ApiProperty({ type: Boolean })
   active: boolean;
 
+  @IsNotEmpty()
   @ApiProperty({ type: Date })
   created_at: Date;
 
